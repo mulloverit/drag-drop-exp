8c6235cd60fd1b87e223760a4f0ab4ddccc6b64a
mulloverit <camillamahon@fastmail.com>
2019-02-05 13:42:29 -0800

    cleaning up env, nenv, and requirements

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_index():
+	"""Show index page"""
+
+	return render_template("index.html")
+
+@app.route("/draggable")
+def draggable_experiments():
+	"""Experimenting with Draggable.js library"""
+
+	return render_template("draggable.html")
+
+@app.route("/interactive")
+def interactive_experiments():
+	"""Experiementing with Interactive.js library"""
+
+	return render_template("interactive.html")
+
+if __name__ == "__main__":
+	app.run(debug=True, host="0.0.0.0")
---
+++ templates/import-droppable.js
@@ -0,0 +1,2 @@
+import { Droppable } from '@shopify/draggable';
+
